5a7b9c66b9122c5299af886d50c2cc3b0c22aa84
Garrett <garrettswink421@gmail.com>
2023-08-15 19:36:01 -0400

    Initial server commits

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+// Import express, set up port, and import routes
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const htmlRoute = require('./routes/html');
+const apiRoute = require('./routes/api');
+
+// Middleware
+// Set URL encded format; Set to false to use query string library, excludes more complex objects
+// Parse Json
+// Serve static files from public folder
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+app.use('/', htmlRoute);
+app.use('/api', apiRoute);
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
