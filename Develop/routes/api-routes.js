fad7297d8f30f25124eac1ccbc6dd59189196b5a
Garrett <garrettswink421@gmail.com>
2023-08-16 10:48:58 -0400

    API Routing Added + TroubleShooting App

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+router.get('/api/notes', async (req, res) => {
+    const Usernote = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(Usernote);
+});
+
+router.post('/api/notes', (req, res) => {
+    const Usernote = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+    };
+    
+    Usernote.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(Usernote));
+    res.json(Usernote);
+});
