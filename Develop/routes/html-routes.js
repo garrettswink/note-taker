7c4edca5d5d7bad99afeeb2711c17c3f5c1ea99c
Garrett <garrettswink421@gmail.com>
2023-08-15 20:26:42 -0400

    HTML Routing

---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const path = require('path');
+
+// Is this one needed? Seems like it's just for inputs on the first page, which should be handled by a link...return to this.
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
